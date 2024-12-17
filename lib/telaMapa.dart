97508cdfb77e4b674c65659f7b281777c149d699
GabrielTembra <112914421+GabrielTembra@users.noreply.github.com>
2024-12-17 17:55:34 -0300

    novo commit

--- lib/telaMapa.dart
+++ lib/telaMapa.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:html' as html; // Importa o pacote HTML para manipular o iframe
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'dart:html' as html;
 
 void main() {
-  setUrlStrategy(PathUrlStrategy());  // Permite que o navegador use URLs "limpas" sem hashes.
   runApp(const MyApp());
 }
 
@@ -27,34 +25,21 @@ class MusicMapScreen extends StatefulWidget {
 }
 
 class _MusicMapScreenState extends State<MusicMapScreen> {
-  // Lista de locais estáticos de música
   final List<Map<String, dynamic>> musicLocations = [
-    {
-      'name': 'Local 1',
-      'latitude': 51.5074,
-      'longitude': -0.1278,
-    },
-    {
-      'name': 'Local 2',
-      'latitude': 51.5155,
-      'longitude': -0.1420,
-    },
-    {
-      'name': 'Local 3',
-      'latitude': 51.5194,
-      'longitude': -0.1340,
-    },
-    // Adicione mais locais conforme necessário
+    {'name': 'Local 1', 'latitude': 51.5074, 'longitude': -0.1278},
+    {'name': 'Local 2', 'latitude': 51.5155, 'longitude': -0.1420},
+    {'name': 'Local 3', 'latitude': 51.5194, 'longitude': -0.1340},
   ];
 
-  // Gerar o HTML com mapa e locais
   String _generateMapHTML() {
-    String markers = "";
+    String markers = '';
     for (var location in musicLocations) {
       markers += """
-        L.marker([${location['latitude']}, ${location['longitude']}])
-          .addTo(map)
-          .bindPopup('<b>${location['name']}</b><br>Lat: ${location['latitude']}<br>Lon: ${location['longitude']}')
+        new google.maps.Marker({
+          position: {lat: ${location['latitude']}, lng: ${location['longitude']}},
+          map: map,
+          title: '${location['name']}'
+        });
       """;
     }
 
@@ -63,41 +48,37 @@ class _MusicMapScreenState extends State<MusicMapScreen> {
       <html>
       <head>
         <title>Music Map</title>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCdXPkH0L5txsBvmI0FAOepZpMWufrpIRY&callback=initMap" async defer></script>
         <script>
-          var map = L.map('map').setView([51.509, -0.128], 12); // Posição inicial do mapa (Londres)
+          var map;
+          function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+              center: {lat: 51.5074, lng: -0.1278},
+              zoom: 13,
+            });
 
-          // Adicionar tiles do OpenStreetMap
-          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          }).addTo(map);
-
-          // Adicionar marcadores
-          $markers
+            // Adicionar marcadores
+            $markers
+          }
         </script>
       </head>
       <body>
-        <div id="map" style="height: 100%;"></div>
+        <div id="map" style="height: 100%; width: 100%;"></div>
       </body>
       </html>
     ''';
   }
 
-  // Registrar o HTML do mapa no navegador
   @override
   void initState() {
     super.initState();
-
-    // Adicionar o iframe com o conteúdo gerado pelo HTML
     final iframeElement = html.IFrameElement()
-      ..srcdoc = _generateMapHTML()  // Insira o HTML gerado aqui
+      ..srcdoc = _generateMapHTML()
       ..style.border = 'none'
       ..width = '100%'
-      ..height = '100%';
-    
-    // Registrar o viewType para ser utilizado no HtmlElementView
-    html.document.getElementById('map')?.append(iframeElement);
+      ..height = '500px';
+
+    html.document.body!.append(iframeElement);
   }
 
   @override
@@ -113,9 +94,7 @@ class _MusicMapScreenState extends State<MusicMapScreen> {
           child: SizedBox(
             width: double.infinity,
             height: 500,  // Ajuste o tamanho conforme necessário
-            child: HtmlElementView(
-              viewType: 'map',  // Define o viewType registrado
-            ),
+            child: HtmlElementView(viewType: 'map'),
           ),
         ),
       ),
