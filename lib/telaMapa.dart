3b46be7ef5d101a2cd556b4ab125c6d142132719
GabrielTembra <112914421+GabrielTembra@users.noreply.github.com>
2024-12-25 12:13:27 -0300

    commit novo

--- lib/telaMapa.dart
+++ lib/telaMapa.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:provider/provider.dart';
+import 'package:trackonnections/profile_provider.dart';
 
 class MusicMapScreen extends StatefulWidget {
   const MusicMapScreen({Key? key}) : super(key: key);
@@ -17,69 +19,129 @@ class _MusicMapScreenState extends State<MusicMapScreen> with AutomaticKeepAlive
   final List<CircleMarker> _circleMarkers = [];
 
   double _currentZoom = 12.0; // Zoom inicial
+  final double _minZoom = 3.0; // Zoom mínimo permitido
+  final double _maxZoom = 18.0; // Zoom máximo permitido
 
   @override
   void initState() {
     super.initState();
     mapController = MapController();
-    _initializeLocation(); // Tenta inicializar a localização assim que a tela for carregada
+    _initializeLocation(); // Inicializa a localização assim que a tela for carregada
   }
 
-  // Função para inicializar a localização
   Future<void> _initializeLocation() async {
     await _getDeviceLocation();
   }
 
-  // Função para obter a localização do dispositivo
   Future<void> _getDeviceLocation() async {
-    if (await Geolocator.isLocationServiceEnabled()) {
-      try {
-        Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high,
-        );
+    if (!await Geolocator.isLocationServiceEnabled()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Serviço de localização desativado. Ative-o para usar o mapa.')));
 
-        setState(() {
-          _currentLocation = LatLng(position.latitude, position.longitude);
-          _markers.clear();
-          _markers.add(
-            Marker(
-              point: _currentLocation!,
-              width: 80,
-              height: 80,
+      return;
+    }
+
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      setState(() {
+        _currentLocation = LatLng(position.latitude, position.longitude);
+
+        // Atualiza os marcadores
+        _markers.clear();
+        _markers.add(
+          Marker(
+            point: _currentLocation!,
+            width: 80,
+            height: 80,
+            child: GestureDetector(
+              onTap: () {
+                _showMusicDialog();
+              },
               child: Container(
                 width: 20,
                 height: 20,
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
-                  color: Colors.blue.withOpacity(0.15),
+                  color: Colors.purpleAccent.withOpacity(0.7),
+                ),
+                child: const Icon(
+                  Icons.music_note,
+                  size: 16,
+                  color: Colors.white,
                 ),
               ),
             ),
-          );
+          ),
+        );
 
-          _circleMarkers.clear();
-          _circleMarkers.add(
-            CircleMarker(
-              point: _currentLocation!,
-              color: Colors.blue.withOpacity(0.15),
-              borderColor: Colors.blue.withOpacity(0.15),
-              borderStrokeWidth: 2,
-              useRadiusInMeter: true,
-              radius: 100, // Raio em metros
-            ),
-          );
-        });
+        // Atualiza os círculos
+        _circleMarkers.clear();
+        _circleMarkers.add(
+          CircleMarker(
+            point: _currentLocation!,
+            color: Colors.purpleAccent.withOpacity(0.15),
+            borderColor: Colors.purpleAccent.withOpacity(0.15),
+            borderStrokeWidth: 2,
+            useRadiusInMeter: true,
+            radius: 100, // Raio em metros
+          ),
+        );
 
-        // Atualiza a posição da câmera para a localização atual
+        // Move o mapa para a localização atual
         mapController.move(_currentLocation!, _currentZoom);
-      } catch (e) {
-        // Em vez de exibir o pop-up, você pode tratar o erro de forma silenciosa ou logá-lo
-        print('Erro ao obter a localização: $e');
-      }
-    } else {
-      // Em vez de mostrar o pop-up, você pode apenas logar ou agir de outra maneira
-      print('Serviço de localização desativado');
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao obter a localização: $e')))
+      ;
     }
+  }
+
+  void _showMusicDialog() {
+    final profileProvider = Provider.of<ProfileProvider>(context, listen: false);
+
+    // Atualiza a música atual no perfil
+    profileProvider.setCurrentlyPlayingTrack(
+      profileProvider.currentSongArtist,
+      profileProvider.currentSongName,
+      profileProvider.albumArtUrl as Map<String, dynamic>,
+    );
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Música Tocando"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                profileProvider.currentSongName != null && profileProvider.currentSongArtist != null
+                    ? "${profileProvider.currentSongName} - ${profileProvider.currentSongArtist}"
+                    : "Nenhuma música tocando",
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 10),
+              const Icon(
+                Icons.music_note,
+                size: 48,
+                color: Colors.purpleAccent,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Fechar"),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -92,8 +154,10 @@ class _MusicMapScreenState extends State<MusicMapScreen> with AutomaticKeepAlive
           FlutterMap(
             mapController: mapController,
             options: MapOptions(
-              initialCenter: _currentLocation ?? LatLng(0.0, 0.0),
-              minZoom: _currentZoom, // Defina o nível de zoom inicial
+              initialCenter: _currentLocation ?? LatLng(0.0, 0.0), // Corrigido para center
+              initialZoom: _currentZoom, // Corrigido para zoom
+              minZoom: _minZoom,
+              maxZoom: _maxZoom,
             ),
             children: [
               TileLayer(
@@ -108,7 +172,6 @@ class _MusicMapScreenState extends State<MusicMapScreen> with AutomaticKeepAlive
               ),
             ],
           ),
-          // Adicionando os ícones de zoom sem BottomAppBar
           Positioned(
             bottom: 20,
             left: 20,
@@ -116,23 +179,31 @@ class _MusicMapScreenState extends State<MusicMapScreen> with AutomaticKeepAlive
               children: [
                 IconButton(
                   icon: const Icon(Icons.zoom_in),
-                  color: Colors.black, // Cor personalizada para o ícone de zoom in
+                  color: Colors.black,
                   onPressed: () {
-                    setState(() {
-                      _currentZoom++;
-                    });
-                    mapController.move(_currentLocation!, _currentZoom); // Ajuste do zoom
+                    if (_currentZoom < _maxZoom) {
+                      setState(() {
+                        _currentZoom++;
+                      });
+                      if (_currentLocation != null) {
+                        mapController.move(_currentLocation!, _currentZoom);
+                      }
+                    }
                   },
                 ),
-                const SizedBox(height: 10), // Espaço entre os ícones
+                const SizedBox(height: 10),
                 IconButton(
                   icon: const Icon(Icons.zoom_out),
-                  color: Colors.black, // Cor personalizada para o ícone de zoom out
+                  color: Colors.black,
                   onPressed: () {
-                    setState(() {
-                      _currentZoom--;
-                    });
-                    mapController.move(_currentLocation!, _currentZoom); // Ajuste do zoom
+                    if (_currentZoom > _minZoom) {
+                      setState(() {
+                        _currentZoom--;
+                      });
+                      if (_currentLocation != null) {
+                        mapController.move(_currentLocation!, _currentZoom);
+                      }
+                    }
                   },
                 ),
               ],
